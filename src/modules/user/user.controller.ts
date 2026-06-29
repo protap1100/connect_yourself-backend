0efbf8cabfbf65f4f6324f61f3015d2592fb3494
protap1100 <protapb23@gmail.com>
2026-06-29 20:30:39 +0600

    try catch utilites

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,9 +1,35 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, RequestHandler, Response } from "express";
 import httpsStatus from "http-status";
 import { userServices } from "./user.service";
+import { catchAsync } from "../../utilties/catchAsync";
 
-const createUser = async (req: Request, res: Response) => {
-  try {
+
+
+// const createUser = async (req: Request, res: Response) => {
+//   try {
+//     const payload = req.body;
+//     const user = await userServices.registerUserIntoDB(payload);
+//     res.status(httpsStatus.CREATED).json({
+//       success: true,
+//       statusCode: httpsStatus.CREATED,
+//       message: "user registered successfully",
+//       data: {
+//         user,
+//       },
+//     });
+//   } catch (error) {
+//     console.log(error);
+//     res.status(httpsStatus.INTERNAL_SERVER_ERROR).json({
+//       success: false,
+//       statusCode: httpsStatus.INTERNAL_SERVER_ERROR,
+//       message: "Failed to register user",
+//       error: (error as Error).message,
+//     });
+//   }
+// };
+
+const createUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     const payload = req.body;
     const user = await userServices.registerUserIntoDB(payload);
     res.status(httpsStatus.CREATED).json({
@@ -14,16 +40,8 @@ const createUser = async (req: Request, res: Response) => {
         user,
       },
     });
-  } catch (error) {
-    console.log(error);
-    res.status(httpsStatus.INTERNAL_SERVER_ERROR).json({
-      success: false,
-      statusCode: httpsStatus.INTERNAL_SERVER_ERROR,
-      message: "Failed to register user",
-      error: (error as Error).message,
-    });
-  }
-};
+  },
+);
 
 export const userController = {
   createUser,
---
+++ src/utilties/catchAsync.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, RequestHandler, Response } from "express";
+import httpsStatus from "http-status";
+
+export const catchAsync = (fn: RequestHandler) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await fn(req, res, next);
+    } catch (error) {
+      res.status(httpsStatus.INTERNAL_SERVER_ERROR).json({
+        success: false,
+        statusCode: httpsStatus.INTERNAL_SERVER_ERROR,
+        message: "Failed to register user",
+        error: (error as Error).message,
+      });
+    }
+  };
+};
