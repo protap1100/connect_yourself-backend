966caa85535051a2764d2dd9a0d3214ad1c186f0
protap1100 <protapb23@gmail.com>
2026-07-06 23:22:03 +0600

    feat: add interfaces for comment creation, updating, and moderation

---
+++ src/modules/comment/comment.interface.ts
@@ -0,0 +1,16 @@
+import { commentStatus } from "../../../generated/prisma/enums";
+
+export interface ICreateCommentPayload {
+    postId: string;
+    authorId: string;
+    content: string;
+}
+
+export interface IUpdateCommentPayload { 
+    content ?: string, 
+    status ?: commentStatus 
+}
+
+export interface IModerateCommentPayload {
+    status: commentStatus
+}
