e464f1e425ed218a26855d6bd62826364997396a
protap1100 <protapb23@gmail.com>
2026-07-18 01:46:55 +0600

    feat: enhance global error handler with detailed error responses; refactor catchAsync utility

--- src/utilties/catchAsync.ts
+++ src/utilties/catchAsync.ts
@@ -1,5 +1,4 @@
 import { NextFunction, Request, RequestHandler, Response } from "express";
-import httpsStatus from "http-status";
 
 export const catchAsync = (fn: RequestHandler) => {
   return async (req: Request, res: Response, next: NextFunction) => {
